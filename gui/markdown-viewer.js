e639777b1d6c69dbfc8147dba8a8792ab2390fa9
Erwan Guyader <erwan.guyader@cozycloud.cc>
2020-02-10 17:22:58 +0100

    gui: Add Markdown viewer window
      We want to be able to display the markdown content of Cozy Notes we
      could not open in the web applications (e.g. because the computer is
      oflline).
    
      We'll use `react-markdown` to parse and display an HTML version of the
      markdown content so we create a new HTML base that will load a special
      javascript file which will import React and the other required
      components.
      The content of the note, its filename and other information like the
      title and details of the error that lead us to open the viewer will be
      passed via an asynchronous message sent to the window itself.
      The error will be displayed in a banner at the top of the window so
      the user knows why we did not open the web application and displayed
      the note's content instead.

---
+++ gui/markdown-viewer.js
@@ -0,0 +1,74 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+const ReactMarkdown = require('react-markdown')
+const e = React.createElement
+
+class Banner extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      closed: false
+    }
+
+    this.close = this.close.bind(this)
+  }
+
+  close() {
+    this.setState({ closed: true })
+  }
+
+  render() {
+    const { translate, title, details } = this.props
+    const { closed } = this.state
+    const className = closed ? 'banner banner--closed' : 'banner'
+
+    return e('div', { className }, [
+      e('div', { className: 'banner__main' }, [
+        e(
+          'span',
+          { className: 'banner__main-title' },
+          translate('MarkdownViewer Why do I see this?')
+        ),
+        e('span', { className: 'banner__error-title' }, title)
+      ]),
+      e('p', { className: 'banner__error-details' }, details),
+      e(
+        'div',
+        {
+          className: 'banner__close',
+          onClick: this.close
+        },
+        '\u2715'
+      )
+    ])
+  }
+}
+
+class MarkdownViewer extends React.Component {
+  render() {
+    const { content: source, filename } = this.props
+
+    return e('div', { className: 'markdown-viewer' }, [
+      e('header', { className: 'markdown-viewer__filename' }, [
+        e('h1', null, filename)
+      ]),
+      e(ReactMarkdown, { source }, null)
+    ])
+  }
+}
+
+require('electron').ipcRenderer.on(
+  'load-content',
+  (event, { translations, banner, content, filename }) => {
+    const translate = key =>
+      translations[key] || key.substr(key.indexOf(' ') + 1) // Key without prefix
+
+    ReactDOM.render(
+      e(React.Fragment, null, [
+        e(Banner, { ...banner, translate }, null),
+        e(MarkdownViewer, { content, filename }, null)
+      ]),
+      document.getElementById('container')
+    )
+  }
+)
